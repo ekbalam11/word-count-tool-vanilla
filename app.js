a4c299e3a9e1518d9e52af8b69db8b709b90d5b1
ekbalam11 <ekbalam11@gmail.com>
2024-05-08 20:46:54 +0200

    simple excercise done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // Buena suerte
+const  input = document.querySelector('#text');
+
+input.addEventListener('input', function (event){
+    let phraseLength = event.target.value.length;
+    console.log(phraseLength);
+
+    document.querySelector('#text-length').textContent = phraseLength;
+});
